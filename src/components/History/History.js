f95248ca1e836a7f870ecbdd6fba5fdafd0cf289
Luke Donahue <luke9389@gmail.com>
2019-11-04 09:57:00 -0800

    initail setup and some skeletal components

---
+++ src/components/History/History.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+export default class History extends Component {
+
+  render() {
+    return (
+      <aside>
+        <h2>History</h2>
+        <ul id='history'>
+
+        </ul>
+      </aside>
+    );
+  }
+}
---
+++ src/components/Main/Main.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import History from '../History/History';
+
+function Main() {
+  return (
+    <main>
+      <History />
+      <section className="deck">
+        
+      </section>
+    </main>
+  );
+}
+
+export default Main;
